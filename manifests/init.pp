7ca700f51f349ec82bdbce663d8f07b13da7321a
Christopher Webber <cwebber@ucr.edu>
2011-08-31 20:52:54 -0700

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,53 @@
+class mwf {}
+
+class mwf::server ($site_url, $site_assets_url, $site_nonmobile_url = false) {
+   
+   package {"git": ensure => installed }
+   package {"httpd": ensure => installed }
+   package {"php": ensure => installed}
+   package {"php-common": ensure => installed }
+   package {"php-devel": ensure => installed }
+   package {"php-cli": ensure => installed }
+   package {"php-gd": ensure => installed }
+   package {"php-xml": ensure => installed }
+
+   service {"httpd":
+      ensure => running,
+      enable => true,
+      hasrestart => true,
+      hasstatus => true,
+      require => Package['httpd', 'php', 'php-common', 'php-devel', 'php-cli', 'php-gd', 'php-xml']
+   }
+
+   exec {"/usr/bin/git clone https://github.com/ucla/mwf.git":
+      alias => "git_clone",
+      creates => "/home/mwf",
+      cwd => "/home",
+      notify => Exec['install'],
+      require => Package['git']
+   }
+
+   exec {"/home/mwf/install/install.sh":
+      alias => "install",
+      refreshonly => true,
+      require => Exec['git_clone'],
+      notify => Exec['install_wurfl']
+   }
+
+   exec {"/home/mwf/install/install-wurfl-api.sh":
+      alias => "install_wurfl",
+      refreshonly => true,
+      require => Exec['install']
+   }
+
+   file {"/var/www/html/mobile":
+      ensure => "/home/mwf/root",
+      require => Exec['git_clone']
+   }
+
+   file {"/home/mwf/config/global.php":
+      ensure => present,
+      content => template("mwf/global.php.erb"),
+      require => Exec['install']
+   }
+}
